0b4bde1aa61f6c56cd3da32ba790da00ea7b2b12
BKrasnov <bkrasnov235@gmail.com>
2023-01-27 22:53:37 +0700

    Create reducers currencyList KT-1

---
+++ src/store/reducers/currency.ts
@@ -0,0 +1,48 @@
+import {Currency} from 'core/utils/currencyList';
+import {CurrencyAction, CurrencyActionTypes} from 'store/types/currency';
+import {RootState} from '.';
+
+/** Стейт списка валют. */
+interface CurrencyListState {
+    /** Загружается ли валюта в данный момент. */
+    readonly isLoading?: boolean;
+
+    /** Список полученных валют. */
+    readonly currencyList: Currency[];
+
+    /** Ошибка при загрузке списка валют. */
+    readonly error?: string;
+}
+
+const initialState: CurrencyListState = {
+    isLoading: false,
+    currencyList: [],
+    error: undefined,
+};
+
+export const currencyList = (state = initialState, action: CurrencyAction): CurrencyListState => {
+    switch (action.type) {
+        case CurrencyActionTypes.FETCH_PENDING:
+            return {
+                ...state,
+                isLoading: true,
+            };
+        case CurrencyActionTypes.FETCH_SUCCESS:
+            return {
+                ...state,
+                isLoading: false,
+                currencyList: action.payload,
+            };
+        case CurrencyActionTypes.FETCH_ERROR:
+            return {
+                ...state,
+                isLoading: false,
+                error: action.payload,
+            };
+        default:
+            return state;
+    }
+};
+
+export const selectorCurrencyList = (state: RootState) => state.currency.currencyList;
+export const selectorCurrencyListIsLoading = (state: RootState) => state.currency.isLoading;
