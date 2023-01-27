e4fee3da0ada07690fa5d111baca540efe0465bf
BKrasnov <bkrasnov235@gmail.com>
2023-01-27 23:01:36 +0700

    Add currencyList to CurrencyTable KT-1

--- src/routes/IndexPage/views/CurrencyTable/CurrencyTable.tsx
+++ src/routes/IndexPage/views/CurrencyTable/CurrencyTable.tsx
@@ -1,10 +1,51 @@
 import * as React from 'react';
-
 import './CurrencyTable.scss';
-import {useBem} from '@steroidsjs/core/hooks';
+import {useBem, useDispatch, useSelector} from '@steroidsjs/core/hooks';
+import getCurrencyList from 'store/actions/currency';
+import {selectorCurrencyList, selectorCurrencyListIsLoading} from 'store/reducers/currency';
 
 export default function CurrencyTable() {
     const bem = useBem('CurrencyTable');
+    const dispatch = useDispatch();
 
-    return <div className={bem.block()}>Table</div>;
+    const currencyList = useSelector(selectorCurrencyList);
+    const isLoading = useSelector(selectorCurrencyListIsLoading);
+
+    React.useEffect(() => {
+        dispatch(getCurrencyList(1, ['RUB', 'USD', 'EUR', 'CNY']));
+    }, [dispatch]);
+
+    return (
+        <div className={bem.block()}>
+            {isLoading ? (
+                <div>Загрузка...</div>
+            ) : (
+                currencyList?.map((item, index) => (
+                    <div>
+                        <div>{item.label}</div>
+                    </div>
+                ))
+            )}
+            {/* <Grid
+                listId="GridLoadMoreDemo"
+                items={[
+                    {id: 1, name: 'USD', iso: 23432},
+                    {id: 2, name: 'RUB'},
+                ]}
+                columns={[
+                    {label: '№', attribute: 'number'},
+                    {label: 'ISO', attribute: 'iso'},
+                    {label: 'Name', attribute: 'name'},
+                    {label: 'Value', attribute: 'value'},
+                ]}
+                pagination={{
+                    loadMore: true,
+                }}
+                paginationSize={{
+                    defaultValue: 2,
+                    sizes: [2, 3, 4],
+                }}
+            /> */}
+        </div>
+    );
 }
