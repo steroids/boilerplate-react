7fe8d27fb280f6efdc8b5a0ed222b66fa518a7f3
BKrasnov <bkrasnov235@gmail.com>
2023-01-31 23:23:10 +0700

    Add uesMemo to CurrencyTable KT-4

--- src/routes/IndexPage/views/CurrencyTable/CurrencyTable.tsx
+++ src/routes/IndexPage/views/CurrencyTable/CurrencyTable.tsx
@@ -10,10 +10,10 @@ import getCurrencyTable from './utils';
 /** Дефолтные параметры для запроса. Можно менять. */
 export const DEFAULT_QUERY_PARAMS: IListQueryParams = {
     currencies: ['RUB', 'USD', 'EUR', 'CNY'],
-    numberOfCurrencies: 2,
+    numberOfCurrencies: 25,
 };
 
-const DEFAULT_PAGINATION_SIZE = 3;
+const DEFAULT_PAGINATION_SIZE = 4;
 
 export default function CurrencyTable() {
     const bem = useBem('CurrencyTable');
@@ -24,6 +24,17 @@ export default function CurrencyTable() {
 
     const table = getCurrencyTable('currencyTable', DEFAULT_QUERY_PARAMS.currencies);
 
+    const items = React.useMemo(
+        () =>
+            currencyList.map((currency) => ({
+                id: currency.id,
+                iso: currency.iso,
+                label: currency.label,
+                ...currency.rates,
+            })),
+        [currencyList],
+    );
+
     React.useEffect(() => {
         if (currencyList.length) {
             return;
@@ -32,29 +43,27 @@ export default function CurrencyTable() {
     }, [currencyList.length, dispatch]);
 
     return (
-        <div className={bem.block()}>
+        <>
             {isLoading ? (
                 <div>Загрузка...</div>
             ) : (
-                <Grid
-                    className={bem.element('grid')}
-                    listId={table.listId}
-                    items={currencyList.map((currency) => ({
-                        id: currency.id,
-                        iso: currency.iso,
-                        label: currency.label,
-                        ...currency.rates,
-                    }))}
-                    columns={table.columns}
-                    paginationSize={{
-                        defaultValue: DEFAULT_PAGINATION_SIZE,
-                        sizes: [],
-                    }}
-                    pagination={{
-                        loadMore: true,
-                    }}
-                />
+                <div className={bem.block()}>
+                    <Grid
+                        className={bem.element('grid')}
+                        listId={table.listId}
+                        items={items}
+                        columns={table.columns}
+                        paginationSize={{
+                            defaultValue: DEFAULT_PAGINATION_SIZE,
+                            sizes: [],
+                        }}
+                        pagination={{
+                            loadMore: true,
+                        }}
+                        sort={{enable: true, defaultValue: 'id'}}
+                    />
+                </div>
             )}
-        </div>
+        </>
     );
 }
