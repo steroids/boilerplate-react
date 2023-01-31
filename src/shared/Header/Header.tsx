7efe379328d9f7d42ae592fe27a7eb565a5f302a
BKrasnov <bkrasnov235@gmail.com>
2023-01-31 16:16:18 +0700

    Create Header component KT-3

---
+++ src/shared/Header/Header.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import './Header.scss';
+import {Button} from '@steroidsjs/core/ui/form';
+import {useBem, useDispatch, useSelector} from '@steroidsjs/core/hooks';
+import getCurrencyList from 'store/actions/currency';
+import {selectCurrencyList, selectCurrencyListIsLoading} from 'store/reducers/currency';
+import {DEFAULT_QUERY_PARAMS} from '../../routes/IndexPage/views/CurrencyTable/CurrencyTable';
+
+const MILLISECOND = 1000;
+
+export default function Header() {
+    const bem = useBem('Header');
+    const dispatch = useDispatch();
+
+    const currencyList = useSelector(selectCurrencyList);
+    const isLoading = useSelector(selectCurrencyListIsLoading);
+
+    const date = new Date(currencyList[0]?.timestamp * MILLISECOND);
+
+    const handleClick = () => {
+        dispatch(getCurrencyList(DEFAULT_QUERY_PARAMS));
+    };
+
+    return (
+        <header className={bem.block()}>
+            <h3>Конвертер валют</h3>
+            <span>{currencyList[0]?.timestamp ? <>{date.toString()}</> : <>Нет данных об обновлении</>}</span>
+
+            <Button color='Primary' label='Обновить данные' onClick={handleClick} disabled={isLoading} />
+        </header>
+    );
+}
