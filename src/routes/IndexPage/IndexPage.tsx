bc99a3eeef47b6645a0bc281e852bae73a91162e
Daria Kuzminykh <ds.kuzminyh@yandex.ru>
2023-02-15 14:35:56 +0700

    Upgrade to steroids v3

--- src/routes/IndexPage/IndexPage.tsx
+++ src/routes/IndexPage/IndexPage.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
+import {useBem} from '@steroidsjs/core/hooks';
 
 import './IndexPage.scss';
-import {useBem} from '@steroidsjs/core/hooks';
 
 export default function IndexPage() {
     const bem = useBem('IndexPage');
 
     return (
         <div className={bem.block()}>
-            Hello ;)
+            Hello!
         </div>
     );
 }
