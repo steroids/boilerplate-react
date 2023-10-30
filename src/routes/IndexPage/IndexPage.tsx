d5c589693196dac933424c5bf2811bbd86f61339
Daria Kuzminykh <ds.kuzminyh@yandex.ru>
2023-10-30 12:37:22 +0800

    Try use zustand

--- src/routes/IndexPage/IndexPage.tsx
+++ src/routes/IndexPage/IndexPage.tsx
@@ -1,14 +1,38 @@
 import * as React from 'react';
-import {useBem} from '@steroidsjs/core/hooks';
+import {useBem, useDispatch} from '@steroidsjs/core/hooks';
 
 import './IndexPage.scss';
+import Button from '@steroidsjs/core/ui/form/Button/Button';
+import {showNotification} from '@steroidsjs/core/store/notificationsStore';
 
+// Redux
+// export default function IndexPage() {
+//     const bem = useBem('IndexPage');
+
+//     const dispatch = useDispatch();
+
+//     return (
+//         <div className={bem.block()}>
+//             Hello!
+//             <Button
+//                 label="Show notification"
+//                 onClick={() => dispatch(showNotification('Success'))}
+//             />
+//         </div>
+//     );
+// }
+
+// // Zustand
 export default function IndexPage() {
     const bem = useBem('IndexPage');
 
     return (
         <div className={bem.block()}>
             Hello!
+            <Button
+                label="Show notification"
+                onClick={() => showNotification('Success')}
+            />
         </div>
     );
 }
--- webpack.js
+++ webpack.js
@@ -1,4 +1,17 @@
+const path = require('path');
+
+const STEROIDS_CORE_PATH = '../../../Steroids/dev/steroids/react/src';
+const STEROIDS_BOOTSTRAP_PATH = '../../../Steroids/dev/steroids/react-bootstrap/src';
+
 require('@steroidsjs/webpack').config({
     inlineSvg: true,
     port: 9991,
+    webpack: {
+        resolve: {
+            alias: {
+                '@steroidsjs/core': path.resolve(__dirname, STEROIDS_CORE_PATH),
+                '@steroidsjs/bootstrap': path.resolve(__dirname, STEROIDS_BOOTSTRAP_PATH),
+            },
+        },
+    },
 });
