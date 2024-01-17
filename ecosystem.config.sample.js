43344dbef64d670a8b87cdb99c91b0879ecb7d65
perlexed <perlexed@gmail.com>
2024-01-17 12:12:17 +0700

    README.md, CI/CD fixed

---
+++ ecosystem.config.sample.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apps: [{
+        name: 'boilerplate12345-ssr',
+        script: 'public/server.js',
+        cwd: '/var/www/boilerplate12345',
+        error_file: '/var/www/boilerplate12345/logs/ssr-error.log',
+        out_file: '/var/www/boilerplate12345/logs/ssr-out.log',
+    }],
+};
