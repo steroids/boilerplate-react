a3f37787a055f1ed71cd4ecc73543390d78f58a3
Konstantin Smirnov <smirnov.konstantiin@gmail.com>
2024-03-01 13:22:48 +0700

    Init

--- src/routes/IndexPage/IndexPage.tsx
+++ src/routes/IndexPage/IndexPage.tsx
@@ -2,13 +2,24 @@ import * as React from 'react';
 import {useBem} from '@steroidsjs/core/hooks';
 
 import './IndexPage.scss';
+import Spline from '@splinetool/react-spline';
 
 export default function IndexPage() {
     const bem = useBem('IndexPage');
 
     return (
         <div className={bem.block()}>
-            Hello!
+            <Spline
+                scene="https://prod.spline.design/PRHVU-f8fecoRu8W/scene.splinecode"
+                //scene="https://prod.spline.design/6Wq1Q7YGyM-iab9i/scene.splinecode"
+                style={{
+                    width: '100vw',
+                    height: '100vh',
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                }}
+            />
         </div>
     );
 }
