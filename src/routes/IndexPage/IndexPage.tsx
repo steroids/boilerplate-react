ed1c5721bea7f6b39cecfc12b4947d858de382a8
Dmitriy <kraxtv471365@gmail.com>
2024-04-17 11:07:10 +0700

    Bug env setup

--- src/routes/IndexPage/IndexPage.tsx
+++ src/routes/IndexPage/IndexPage.tsx
@@ -2,13 +2,42 @@ import * as React from 'react';
 import {useBem} from '@steroidsjs/core/hooks';
 
 import './IndexPage.scss';
+import Form from '@steroidsjs/core/ui/form/Form/Form';
+
+const FORM_ID = 'default_form';
 
 export default function IndexPage() {
     const bem = useBem('IndexPage');
 
     return (
         <div className={bem.block()}>
-            Hello!
+            <Form
+                formId={FORM_ID}
+                action="api/v1/user/contest-work/"
+                actionMethod='post'
+                submitLabel={__('Сохранить')}
+                fields={[
+                    {
+                        attribute: 'filesIds',
+                        component: 'FileField',
+                        multiple: true,
+                        uploaderConfig: {
+                            useFormData: true,
+                        },
+                        backendUrl: '/api/v1/file/upload-file',
+                        buttonProps: {
+                            label: __('Добавить\nфото'),
+                        },
+                        uploader: {
+                            queue: {
+                                maxFiles: 5,
+                            },
+                        },
+                        fieldLayoutClassName: bem.element('files'),
+                        hint: __('Можно загрузить не более 5 фотографий'),
+                    },
+                ]}
+            />
         </div>
     );
 }
