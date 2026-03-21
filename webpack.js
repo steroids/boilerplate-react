1460426d63b1eca2c0d18b6f9bfcc28d857d6875
Konstantin Smirnov <smirnov.konstantiin@gmail.com>
2026-03-21 23:17:47 +0700

    Fix webpack

--- webpack.js
+++ webpack.js
@@ -1,12 +1,4 @@
-const getWebpackConfig = () => ({
-    watchOptions: {
-        ignored: /node_modules/,
-        poll: 1000,
-    },
-});
-
 require('@steroidsjs/webpack').config({
     inlineSvg: true,
-    port: process.env.APP_SSR_PORT || 9991,
-    webpack: getWebpackConfig(),
+    port: 9991,
 });
